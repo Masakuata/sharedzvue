b9c1917273fce906762f6415c43a517b9ed4c3a9
“GerardoSoft” <rendon.lusigerardo@gmail.com>
2024-01-15 20:08:30 -0600

    flujos alternos de registrar producto y loadings aniadidos

---
+++ src/components/utilities/SuccesX.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="w-full">
+        <p class="text-2xl font-semibold text-center text-bgBlue">{{ message }}</p>
+        <div class="w-full flex justify-center mt-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentFill"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-check-check w-16 h-auto text-green-600 fill-gray-100">
+                <path d="M18 6 7 17l-5-5" />
+                <path d="m22 10-7.5 7.5L13 16" />
+            </svg>
+        </div>
+        <div class="p-2 w-full">
+            <ButtonX color="blue" @click="emitirAceptar">{{ buttonMessage }}</ButtonX>
+        </div>
+
+    </div>
+</template>
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import ButtonX from '@/components/utilities/ButtonX.vue';
+
+const props = defineProps({
+    message: {
+        type: String,
+        default: 'Éxito...!'
+    },
+    buttonMessage: {
+        type: String,
+        default: 'Aceptar'
+    }
+});
+
+const  emit  = defineEmits(['aceptar']);
+
+const emitirAceptar = () => {
+    emit('aceptar');
+}
+
+</script>
--- src/views/inventory/addProduct/AddProduct.vue
+++ src/views/inventory/addProduct/AddProduct.vue
@@ -1,95 +1,124 @@
 <template>
   <h1 class="text-white absolute top-0 right-0 mr-2   text-xl font-semibold text-left mt-3">AGREGAR PRODUCTO</h1>
   <div @click="clickEnDiv" class="flex flex-col items-center p-4  w-full h-full md:h-full">
-    <div class="w-full">
-      <p class="w-full text-bgPurple font-semibold text-xl text-center">Registra un producto y comienza a registrar sus
-        ventas</p>
+    <template v-if="!requestSent">
+      <div class="w-full">
+        <p class="w-full text-bgPurple font-semibold text-xl text-center">Registra un producto y comienza a registrar sus
+          ventas</p>
 
-      <label for="nombreProducto" class="block text-lg font-medium ">Nombre de producto</label>
-      <input type="text" id="nombreProducto" v-model="nombreProducto" @input="validateNombreProdcuto"
-        class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
-      <template v-if="nombreProducto.length > 0">
-        <p class="text-green-600" v-if="nombreProductoValid">Nombre Válido</p>
-        <p v-else class="text-red-600">El nombre del producto no es válido</p>
-      </template>
-      <p v-else-if="faltaNombreProducto" class="text-red-600">El nombre del producto es un campo requerido</p>
-    </div>
-
-    <div class="w-full">
-      <label for="presentacionProducto" class="block text-lg font-medium ">Presentación</label>
-      <input type="text" id="presentacionProducto" v-model="presentacionProducto" @input="validatePresentacionProdcuto"
-        class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
-      <template v-if="presentacionProducto.length > 0">
-        <p class="text-green-600" v-if="presentacionProductoValid">Presentación válida</p>
-        <p v-else class="text-red-600">La presentación del producto no es válida</p>
-      </template>
-      <p v-else-if="faltaPresentacionProducto" class="text-red-600">La presentacion del producto es un campo requerido</p>
-    </div>
-
-    <div class="flex flex-row items-center text-right">
-      <p class="w-3/4 text-left text-lg font-semibold pr-3">Selecciona la cantidad en stock</p>
-      <input v-model="cantidadProducto" class="w-1/4 h-10 border border-gray-400 rounded-lg mt-2 px-2">
-    </div>
-    <p v-if="faltaCantidadProducto" class="text-red-600 w-full">La cantidad de stock es un campo requerido</p>
-
-    <label for="miSelect" class="block mb-2 text-lg font-semibold w-full text-left">Tipo de mascota</label>
-    <div class="w-full h-12">
-      <select v-model="opcionRazaSeleccionada" id="miSelect"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-        <option disabled value="">Selecciona una opción</option>
-        <option class="w-full" v-for="opcion in opciones" :key="opcion.value" :value="opcion.value">{{ opcion.texto
-        }}</option>
-      </select>
-    </div>
-
-
-
-    <div class="w-full">
-      <label for="precioProducto" class="block text-lg font-medium ">Precio Publico</label>
-      <input type="text" id="precioProducto" v-model="precioProducto" @input="validatePrecioProdcuto"
-        class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
-
-      <p v-if="faltaPrecioProducto" class="text-red-600">El precio del producto es un campo
-        requerido</p>
-    </div>
-
-    <div class="w-full mt-3 border border-gray-500 p-2 rounded-lg">
-      <div class="flex flex-row w-full ">
-        <label class="text-gray-700 mr-10 font-semibold w-1/2" for="checkbox">Precio detalle</label>
-        <div class="w-1/2 text-right">
-          <input type="checkbox" v-model="hasprecioDetalle"
-            class="w-6 h-6 text-blue-600 border-gray-300 rounded focus:ring-blue-500 " id="checkbox">
-        </div>
+        <label for="nombreProducto" class="block text-lg font-medium ">Nombre de producto</label>
+        <input type="text" id="nombreProducto" v-model="nombreProducto" @input="validateNombreProdcuto"
+          class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+        <template v-if="nombreProducto.length > 0">
+          <p class="text-green-600" v-if="nombreProductoValid">Nombre Válido</p>
+          <p v-else class="text-red-600">El nombre del producto no es válido</p>
+        </template>
+        <p v-else-if="faltaNombreProducto" class="text-red-600">El nombre del producto es un campo requerido</p>
       </div>
 
-      <input type="text" :disabled="!hasprecioDetalle" id="precioProducto" v-model="precioDetalle"
-        @input="validatePrecioDetalleProdcuto"
-        class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
-
-      <p v-if="hasprecioDetalle && faltaPrecioDetalle" class="text-red-600 w-full">El precio detalle está activado, espefícica un precio</p>
-
-    </div>
-
-    <div class="w-full mt-3 border border-gray-500 p-2 rounded-lg">
-      <div class="flex flex-row w-full ">
-        <label class="text-gray-700 mr-10 font-semibold w-1/2" for="checkboxMayo">Precio mayorista</label>
-        <div class="w-1/2 text-right">
-          <input type="checkbox" v-model="hasprecioMayorista"
-            class="w-6 h-6 text-blue-600 border-gray-300 rounded focus:ring-blue-500 " id="checkboxMayo">
-        </div>
-        
+      <div class="w-full">
+        <label for="presentacionProducto" class="block text-lg font-medium ">Presentación</label>
+        <input type="text" id="presentacionProducto" v-model="presentacionProducto" @input="validatePresentacionProdcuto"
+          class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+        <template v-if="presentacionProducto.length > 0">
+          <p class="text-green-600" v-if="presentacionProductoValid">Presentación válida</p>
+          <p v-else class="text-red-600">La presentación del producto no es válida</p>
+        </template>
+        <p v-else-if="faltaPresentacionProducto" class="text-red-600">La presentacion del producto es un campo requerido
+        </p>
       </div>
 
-      <input type="text" :disabled="!hasprecioMayorista" id="precioProducto" v-model="precioMayorista"
-        @input="validatePrecioMayoristaProdcuto"
-        class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+      <div class="flex flex-row items-center text-right">
+        <p class="w-3/4 text-left text-lg font-semibold pr-3">Selecciona la cantidad en stock</p>
+        <input v-model="cantidadProducto" class="w-1/4 h-10 border border-gray-400 rounded-lg mt-2 px-2">
+      </div>
+      <p v-if="faltaCantidadProducto" class="text-red-600 w-full">La cantidad de stock es un campo requerido</p>
 
-        <p v-if="hasprecioMayorista && faltaPrecioMayorista" class="text-red-600 w-full">El precio mayorista está activado, espefícica un precio</p>
-    </div>
+      <label for="miSelect" class="block mb-2 text-lg font-semibold w-full text-left">Tipo de mascota</label>
+      <div class="w-full h-12">
+        <select v-model="opcionRazaSeleccionada" id="miSelect"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <option disabled value="">Selecciona una opción</option>
+          <option class="w-full" v-for="opcion in opciones" :key="opcion.value" :value="opcion.value">{{ opcion.texto
+          }}</option>
+        </select>
+      </div>
 
-    <div class="mt-3 w-full">
-      <ButtonX color="purple" @click="registrarProductoMetod">Registrar producto</ButtonX>
-    </div>
+
+
+      <div class="w-full">
+        <label for="precioProducto" class="block text-lg font-medium ">Precio Publico</label>
+        <input type="text" id="precioProducto" v-model="precioProducto" @input="validatePrecioProdcuto"
+          class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+
+        <p v-if="faltaPrecioProducto" class="text-red-600">El precio del producto es un campo
+          requerido</p>
+      </div>
+
+      <div class="w-full mt-3 border border-gray-500 p-2 rounded-lg">
+        <div class="flex flex-row w-full ">
+          <label class="text-gray-700 mr-10 font-semibold w-1/2" for="checkbox">Precio detalle</label>
+          <div class="w-1/2 text-right">
+            <input type="checkbox" v-model="hasprecioDetalle"
+              class="w-6 h-6 text-blue-600 border-gray-300 rounded focus:ring-blue-500 " id="checkbox">
+          </div>
+        </div>
+
+        <input type="text" :disabled="!hasprecioDetalle" id="precioProducto" v-model="precioDetalle"
+          @input="validatePrecioDetalleProdcuto"
+          class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+
+        <p v-if="hasprecioDetalle && faltaPrecioDetalle" class="text-red-600 w-full">El precio detalle está activado,
+          espefícica un precio</p>
+
+      </div>
+
+      <div class="w-full mt-3 border border-gray-500 p-2 rounded-lg">
+        <div class="flex flex-row w-full ">
+          <label class="text-gray-700 mr-10 font-semibold w-1/2" for="checkboxMayo">Precio mayorista</label>
+          <div class="w-1/2 text-right">
+            <input type="checkbox" v-model="hasprecioMayorista"
+              class="w-6 h-6 text-blue-600 border-gray-300 rounded focus:ring-blue-500 " id="checkboxMayo">
+          </div>
+
+        </div>
+
+        <input type="text" :disabled="!hasprecioMayorista" id="precioProducto" v-model="precioMayorista"
+          @input="validatePrecioMayoristaProdcuto"
+          class="w-full mt-1 h-10 px-3 border border-solid border-blueLetters rounded-lg" />
+
+        <p v-if="hasprecioMayorista && faltaPrecioMayorista" class="text-red-600 w-full">El precio mayorista está
+          activado, espefícica un precio</p>
+      </div>
+
+      <div class="mt-3 w-full">
+        <ButtonX color="purple" @click="registrarProductoMetod">Registrar producto</ButtonX>
+      </div>
+    </template>
+    <template v-else>
+      <template v-if="loading">
+        <div class="flex flex-row items-center h-[80vh]">
+          <LoadingX message="Registrando producto..."></LoadingX>
+        </div>
+
+      </template>
+      <template v-else>
+        <template v-if="hasErrror">
+          <div class="flex flex-row items-center h-[80vh]">
+            <ErrorX :message="errorMessage" button-message="Aceptar" @aceptar="aceptarError"> </ErrorX>
+          </div>
+        </template>
+        <template v-else>
+          <div class="flex flex-row items-center h-[80vh] w-full">
+            <SuccesX message="Producto registrado exitósamenta" button-message="Aceptar" @aceptar="aceptar"> </SuccesX>
+          </div>
+        </template>
+
+      </template>
+
+    </template>
+
+
 
 
 
@@ -110,7 +139,10 @@ import { ref, onMounted, watch } from 'vue';
 import { toggleSidebar } from '@/utils/sidebarManager.js';
 import ButtonX from '@/components/utilities/ButtonX.vue';
 import { filtrarEntrada } from '@/utils/validator.js';
-import {registrarProducto} from '@/api/api.js';
+import { registrarProducto } from '@/api/api.js';
+import LoadingX from '@/components/utilities/LoadingX.vue';
+import SuccesX from '@/components/utilities/SuccesX.vue';
+import ErrorX from '@/components/utilities/ErrorX.vue';
 
 
 // Variables y metodos necesarios para tipoMascota
@@ -118,6 +150,11 @@ const opciones = [
   { value: 'perro', texto: 'Perro' },
   { value: 'gato', texto: 'Gato' },
 ];
+
+const requestSent = ref(false);
+const loading = ref(false);
+const hasErrror = ref(false);
+const errorMessage = ref('Error al registrar el producto');
 
 const ID_PUBLICO = 1;
 const ID_DETALLE = 2;
@@ -275,7 +312,7 @@ const validateEmptyfields = () => {
     faltaPrecioDetalle.value = precioDetalle.value.length === 0;
   }
 
-  if( hasprecioMayorista.value){
+  if (hasprecioMayorista.value) {
     faltaPrecioMayorista.value = precioMayorista.value.length === 0;
   }
 
@@ -307,15 +344,26 @@ const validateForm = () => {
   }
 };
 
-const registrarProductoMetod = () => {
+const registrarProductoMetod = async () => {
   if (validateForm()) {
     let payload = buildPayload();
-    
+
     try {
-      let respuesta = registrarProducto(payload);
+      loading.value = true;
+      requestSent.value = true;
+      await registrarProducto(payload);
+
+      loading.value = false;
+
     } catch (error) {
+      hasErrror.value = true;
+      errorMessage.value = error.response.data.detail;
       console.log('Error al registrar el producto', error);
-    
+      loading.value = false;
+
+      if (error.response.status === 409) {
+        errorMessage.value = 'Ya existe un producto con ese nombre y descripción';
+      } 
     }
 
   }
@@ -359,7 +407,28 @@ const buildPayload = () => {
 };
 
 
+const aceptar = () => {
+  loading.value = false;
+  requestSent.value = false;
+  hasErrror.value = false;
+  errorMessage.value = '';
+  precioDetalle.value = '';
+  precioMayorista.value = '';
+  nombreProducto.value = '';
+  presentacionProducto.value = '';
+  cantidadProducto.value = '';
+  precioProducto.value = '';
+  hasprecioDetalle.value = false;
+  hasprecioMayorista.value = false;
+  opcionRazaSeleccionada.value = opciones[0].value;
+};
 
+const aceptarError = () => {
+  loading.value = false;
+  requestSent.value = false;
+  hasErrror.value = false;
+  errorMessage.value = '';
+};
 
 
 
@@ -372,7 +441,7 @@ const clickEnDiv = () => {
 
 onMounted(() => {
   opcionRazaSeleccionada.value = opciones[0].value;
-  //desplazarHaciaElemento();
+
 });
 
 
